56d27aeee7ccda982ace0d135aadd9d5eb29a331
Alex Weissörtel <aweissoertel@uni-koblenz.de>
2021-06-09 15:06:39 +0200

    bla

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { messageHandler } from './components/messageHandler.js';
 
 //const Discord = require('discord.js');
 const client = new Discord.Client();
-client.login('process.env.TOKEN');
+client.login(process.env.TOKEN);
 
 client.once('ready', () => {
 	console.log('Ready!');
